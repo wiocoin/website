064c96a8d08af7ebfb9c68549ecb545df9d61bed
Brothereum <84236781+brothereum@users.noreply.github.com>
2022-05-08 01:23:18 +0530

    some changes

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/wiocoin/website',
+  docsRepositoryBase: 'https://github.com/wiocoin/website/blob/master',
+  titleSuffix: ' – Wiocoin',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Wiocoin (WIO)</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        World is One Coin
       </span>
     </>
   ),
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Wiocoin: the Next.js site builder" />
+      <meta name="og:description" content="Wiocoin: the Next.js site builder" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://wiocoin.vercel.app/og.png" />
+      <meta name="twitter:site:domain" content="wiocoin.vercel.app" />
+      <meta name="twitter:url" content="https://wiocoin.vercel.app" />
+      <meta name="og:title" content="Wiocoin: Next.js static site generator" />
+      <meta name="og:image" content="https://wiocoin.vercel.app/og.png" />
+      <meta name="apple-mobile-web-app-title" content="Wiocoin" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Wiocoin.</>,
   unstable_faviconGlyph: '👋',
 }
